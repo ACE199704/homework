20cf7195c05abd1951e2a5bb2f0d2cd71ff3d87b
Ace1997 <632015598@qq.com>
2019-10-16 17:13:03 +0800

    thrid change

---
+++ css-3-demo/html-demo/main.js
@@ -0,0 +1,16 @@
+// $(".navbar a").on("click", function(e) {
+//   //   console.log(this.hash);
+//   if (this.hash !== "") {
+//     e.preventDefault();
+//     const hash = this.hash;
+//     $("html,body").animate(
+//       {
+//         scrollTop: $(hash).offset().top
+//       },
+//       500
+//     );
+//   }
+// });
+
+// // 第三种方式：smooth-scroll
+// const scroll = new SmoothScroll(".navbar a[href*='#']", { speed: 500 });
