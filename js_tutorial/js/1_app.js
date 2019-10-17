cfabd41e1bf2ed597d5ec7b35f68a5998c081212
Ace1997 <632015598@qq.com>
2019-10-17 22:47:46 +0800

    8

--- js_tutorial/js/1_app.js
+++ js_tutorial/js/1_app.js
@@ -1,43 +1,18 @@
-const person = {
-  firstName: 'Ace',
-  lastName: 'Luo',
-  age: 22,
-  email: 'ace199704@icloud.com',
-  hobbies: ['music', 'sports'],
-  adress: {
-    city: 'Beijing',
-    state: 'zhaoyang'
-  },
-  getBirthday: function () {
-    return 2019 - this.age;
-  }
-};
-
+const today = new Date();
 let val;
+// let brithday = new Date('9-10-2019 11:30:00');
+// let brithday = new Date('September 10 1988');
+let brithday = new Date('9/10/1988');
+val = brithday.getMonth() + 1; //只有月份要加一
+val = brithday.getDate();
+val = brithday.getDay(); //星期几
+val = today.getHours();
+val = today.getMilliseconds();
+val = today.getTime();
 
-val = person;
-//获取单个属性
-val = person.firstName;
-//或者
-val = person['lastName'];
-val = person.email;
-val = person.adress.city;
-val = person.getBirthday();
+brithday.setMonth(2);
+brithday.setDate(22);
+brithday.setFullYear(1922);
+
 console.log(val);
-//数组对象嵌套
-const people = [{
-    name: 'Buckuy',
-    age: 23
-  },
-  {
-    name: 'Henry',
-    age: 30
-  },
-  {
-    name: 'Elyse',
-    age: 20
-  }
-];
-for (let i = 0; i < people.length; i++) {
-  console.log(people[i].name);
-}
+console.log(brithday);
