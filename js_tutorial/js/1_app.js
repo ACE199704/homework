a1e815f4e558e607e8f33513a1951fa57ae15539
Ace1997 <632015598@qq.com>
2019-10-17 14:54:23 +0800

    3

--- js_tutorial/js/1_app.js
+++ js_tutorial/js/1_app.js
@@ -1,43 +1,49 @@
-//类型转换
-let val;
-let num;
-//Number to string
-val = String(533);
-// val = String(4 + 4);
+const num1 = 100;
+const num2 = 50;
 
-//Date to String
-val = String(new Date());
+let val1;
+let val2;
+let val3;
+let val4;
+let val5;
+let val11;
+let val12;
+let val13;
+let val14;
+let val15;
+let val16;
+let val17;
+let val18;
+let val19;
+//数值运算
+val1 = num1 + num2;
+val2 = num1 - num2;
+val3 = num1 * num2;
+val4 = num1 / num2;
+val5 = num1 % num2;
+console.log(val1);
+console.log(val2);
+console.log(val3);
+console.log(val4);
+console.log(val5);
 
-//Array to String
-val = String([1, 2, 3, 4]);
-//toString()
-val = (555).toString();
-val = true.toString();
-//String to Number
-num = Number('5');
-//Bool to Number
-num = Number(true);
-//null to Number
-num = Number(null);
-//NAN
-num = Number('hello');
-//另一种形式：
-num = parseInt('199');
-num = parseInt('199.10');
-num = parseFloat('199.10');
+//math object
+val11 = Math.PI;
+val12 = Math.round(2.1); //四舍五入
+val13 = Math.ceil(2.4); //向上取整
+val14 = Math.floor(2.6); // 向下取整
+val15 = Math.sqrt(4); //开根号
+val16 = Math.abs(-3); //绝对值
+val17 = Math.min(2, 33, 44, -2); //最小值
+val18 = Math.random();
+val19 = Math.floor(Math.random() * 20); //随机数
 
-
-
-//输出
-console.log(val);
-console.log(typeof val);
-//字符串的长度，val.length，number类型的话没有这个函数
-console.log(val.length);
-console.log(num);
-
-console.log(num.toFixed(2));
-
-const val1 = String(5);
-const val2 = 6;
-const sum = val1 + val2;
-console.log(sum);
+console.log(val11);
+console.log(val12);
+console.log(val13);
+console.log(val14);
+console.log(val15);
+console.log(val16);
+console.log(val17);
+console.log(val18);
+console.log(val19);
