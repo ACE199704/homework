29997692473228334678926b1165776280043475
Ace1997 <632015598@qq.com>
2019-10-18 21:47:06 +0800

    changes

--- js_tutorial/js/1_app.js
+++ js_tutorial/js/1_app.js
@@ -1,18 +1,81 @@
-const today = new Date();
-let val;
-// let brithday = new Date('9-10-2019 11:30:00');
-// let brithday = new Date('September 10 1988');
-let brithday = new Date('9/10/1988');
-val = brithday.getMonth() + 1; //只有月份要加一
-val = brithday.getDate();
-val = brithday.getDay(); //星期几
-val = today.getHours();
-val = today.getMilliseconds();
-val = today.getTime();
+//if switch
 
-brithday.setMonth(2);
-brithday.setDate(22);
-brithday.setFullYear(1922);
+//if 分支语句
 
-console.log(val);
-console.log(brithday);
+//条件： 关系运算符 逻辑运算符 最终结果为布尔值
+//关系运算符： > < >= == <= != === !== [ ===不但判断值 也会判断类型 ]
+//逻辑运算符： &&(AND) ||(OR) !(取反)
+
+let id = "100";
+
+if (id !== 100) {
+  console.log("条件为真");
+} else {
+  console.log("条件为假");
+}
+
+// let idd = 100;
+//Test 分支条件是否为undefined
+if (typeof idd !== "undefined") {
+  console.log(`ID是${idd}`);
+} else {
+  console.log("没有设置ID");
+}
+
+const name = "Steve";
+const age = 14;
+if (age > 0 && age <= 12) {
+  console.log(`${name} is a child`);
+} else if (age >= 13 && age <= 19) {
+  console.log(`${name} is a teenager`);
+} else {
+  console.log(`${name} is an adault`);
+}
+//三目运算符
+
+console.log(id === 100 ? "correct" : "Incorrect");
+
+//switch
+const color = "yellow";
+switch (color) {
+  case "red":
+    console.log("color is red");
+    break;
+  case "blue":
+    console.log("color is blue");
+    break;
+  case "yellow":
+    console.log("color is yellow");
+    break;
+  default:
+    console.log("color is unkown");
+    break;
+}
+
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sun";
+    break;
+  case 1:
+    day = "Mon";
+    break;
+  case 2:
+    day = "Tues";
+    break;
+  case 3:
+    day = "Weds";
+    break;
+  case 4:
+    day = "Thri";
+    break;
+  case 5:
+    day = "Fri";
+    break;
+  case 6:
+    day = "Sat";
+    break;
+  default:
+    break;
+}
+console.log(day);
