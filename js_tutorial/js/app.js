034f5958e6cfe2d552cb20c237220c4646223de5
Ace1997 <632015598@qq.com>
2019-10-19 15:12:40 +0800

    problems remained

---
+++ js_tutorial/js/app.js
@@ -0,0 +1,39 @@
+//DOM
+// console.log(document);
+
+let val;
+val = document;
+val = document.all;
+val = document.all[0];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.URL;
+val = document.domain;
+val = document.characterSet;
+val = document.contentType;
+
+val = document.forms;
+val = document.forms[1];
+val = document.forms[1].method;
+val = document.forms[1].action;
+
+val = document.links;
+
+val = document.scripts;
+val = document.scripts[0].getAttribute('src');
+//将对象转化为数组
+let scripts = document.scripts;
+// let scriptsArr = Array.from(scripts);
+// scriptsArr.forEach(function (script) {
+//   console.log(script.getAttribute('src'));
+// })
+console.log(scripts);
+console.log(scripts[0]);
+console.log(scripts[1]);
+
+
+
+// console.log(val);
+// console.log(typeof val);
