165dac5e992be6a1a0d7a471a8df77faa13ead60
Ace1997 <632015598@qq.com>
2019-10-19 11:03:40 +0800

    13

--- js_tutorial/js/1_app.js
+++ js_tutorial/js/1_app.js
@@ -1,56 +1,16 @@
-//Windows 的方法 对象 属性
-// window.console.log("Hello");
+var a = 1;
+let b = 2;
+const c = 3;
 
-//alert
-// window.alert("Hello");
-
-// //prompt
-// const input = prompt();
-// alert(input);
-
-//confirm
-// if (confirm("Are you sure?")) {
-//   console.log("yes");
-// } else {
-//   console.log("no");
-// }
+for (var a = 0; a < 10; a++) {
+  console.log(a);
+  let b = 20;
+  console.log(b);
+  const c = 30;
+  console.log(c);
+}
+console.log(a, b, c);
 
 let val;
-
-//窗口大小
-val = window.outerWidth;
-console.log(val);
-
-//可视区域大小
-val = window.innerWidth;
-console.log(val);
-
-//滚动大小
-val = window.scrollY;
-console.log(val);
-
-//location
-val = window.location;
-val = window.location.hostname;
-val = window.location.port;
-val = window.location.href;
-val = window.location.search;
-
-//Redirect
-// window.location.href = "http://www.baidu.com";
-//Reload
-// window.location.reload();
-
-// //History
-// window.history.go(-1);
-// val = window.history.length;
-
-//Navigator
-val = window.navigator;
-val = window.navigator.appName;
-val = window.navigator.appVersion;
-val = window.navigator.userAgent;
-val = window.navigator.platform;
-val = window.navigator.language;
-
+val = document.all;
 console.log(val);
