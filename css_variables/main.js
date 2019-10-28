24b9e9e5eee39698bd5599c96fe0fa60299a3e75
Ace1997 <632015598@qq.com>
2019-10-28 22:18:51 +0800

    css和js实现分页

--- css_variables/main.js
+++ css_variables/main.js
@@ -6,4 +6,4 @@ const boxBorderColor = boxStyle.getPropertyValue("--box-border-color");
 console.log(boxBorderColor);
 
 const header = document.querySelector("#main-header");
-// header.style.setProperty("--header-bg-color", "#899");
+header.style.setProperty("--header-bg-color", "#899");
