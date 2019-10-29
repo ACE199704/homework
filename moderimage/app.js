6968007f6671c958158a46ba6abc5cccf3381eaa
Ace1997 <632015598@qq.com>
2019-10-29 10:46:35 +0800

    图片显示

---
+++ moderimage/app.js
@@ -0,0 +1,21 @@
+// const smimg = document.querySelectorAll(".img-sm img");
+// const lgimg = document.querySelector(".img-lg img");
+
+// smimg.forEach(img => img.addEventListener("click", imgClick));
+
+// function imgClick(e) {
+//   lgimg.src = e.target.src;
+//   smimg.forEach(img => img.style.opacity = 1);
+//   lgimg.classList.add('fade-in');
+//   setTimeout(() => lgimg.classList.remove('fade-in'), 500);
+//   e.target.style.opacity = 0.4
+// }
+
+const [current, imgs] = [document.querySelector(".img-lg img"), document.querySelectorAll(".img-sm img")];
+imgs.forEach(img => img.addEventListener("click", (e) => {
+  current.src = e.target.src;
+  current.classList.add('fade-in');
+  setTimeout(() => current.classList.remove('fade-in'), 500);
+  imgs.forEach(img => img.style.opacity = 1);
+  e.target.style.opacity = 0.4;
+}))
